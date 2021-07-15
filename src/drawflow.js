61913636cfdc307f14e8a3388bdd9177f7de4789
Denis Glimbovskiy <denis.glimbovskij@gmail.com>
2021-07-15 10:11:31 +0300

    rollback changeModule

--- src/drawflow.js
+++ src/drawflow.js
@@ -1971,17 +1971,15 @@ export default class Drawflow {
   changeModule(name, update) {
     this.dispatch('moduleChanged', name);
     this.module = name;
+	this.precanvas.innerHTML = "";
+	this.canvas_x = 0;
+	this.canvas_y = 0;
     this.pos_x = 0;
     this.pos_y = 0;
     this.mouse_x = 0;
     this.mouse_y = 0;
-	if (!update) {
-		this.precanvas.innerHTML = "";
-		this.canvas_x = 0;
-		this.canvas_y = 0;
-		this.zoom = 1;
-		this.zoom_last_value = 1;
-	}
+	this.zoom = 1;
+	this.zoom_last_value = 1;
 	this.precanvas.style.transform = '';
     this.import(this.drawflow);
   }
